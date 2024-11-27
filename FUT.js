7a74315deb6a7510c6b398080b5a6d90c5ad3d5b
soulayman <soulaymanjaa09@gmail.com>
2024-11-27 11:35:36 +0100

    fixed GKs

--- FUT.js
+++ FUT.js
@@ -12,6 +12,64 @@ function DisplayList(json) {
 
 
     json.players.forEach(players => {
+        if(players.position == "GK"){
+            ListContainer.innerHTML += `
+            <div class="row">
+       <div class="row">
+           <div class="col-md-1"></div>
+           <div class="col-md-11"> 
+       
+            <div class="container-fluid">
+           
+           <div class="BareOfTitles">
+           <div class="row">
+               <div class="col-md-4">
+                       <div class="col-md-12">
+                       <div class="row">
+                           <div class="col-md-4">
+                           <img src="${players.photo}" class="img-fluid" alt="Player Photo" /></div>
+                           <div class="col-md-8">
+                           <p>${players.name}(GK)</p>
+                           <div class=" col-md-2"><img src="${players.flag}" class="img-fluid py-lg-2" alt="country" /></div>
+                            <div class=" col-md-2"><img src="${players.logo}" class="img-fluid py-lg-0" alt="club" /></div>
+                           
+                           </div>
+                           </div>
+                       </div>
+                       </div>
+                       <div class="col-md-8 py-lg-5">
+                           <div class="row">
+                           <div class="col-md-6">
+                               <div class="row">
+                               <div class="col-md-3 d-flex justify-content-center"><p>${players.rating}</p></div>
+                               <div class="col-md-3 d-flex justify-content-center"><p>${players.position}</p></div>
+                               <div class="col-md-3 d-flex justify-content-center"><p>${players.diving}</div>
+                               <div class="col-md-3 d-flex justify-content-center"><p>${players.kicking}</div>
+                               </div>
+                           </div>
+                           <div class="col-md-6">
+                               <div class="row">
+                               <div class="col-md-11">
+                                   <div class="row  d-flex justify-content-center">
+                                   <div class="col-md-3 d-flex justify-content-center"><p>${players.reflexes}</div>
+                                   <div class="col-md-3 d-flex justify-content-center"><p>${players.speed}</div>
+                                   <div class="col-md-3 d-flex justify-content-center"><p>${players.positioning}</div>
+                                   <div class="col-md-3 d-flex justify-content-center"><p>${players.handling}</div>
+                               </div>
+                               </div>
+                               </div>
+                           </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           </div>
+       </div>
+   </div>
+   </div>
+</div>
+       `} else{
         ListContainer.innerHTML += `
              <div class="row">
         <div class="row">
@@ -68,7 +126,7 @@ function DisplayList(json) {
     </div>
     </div>
 </div>
-        `
+        `}
     });
 }
 CallAPI()
