a109d5df0e034851b773826212e9959ca6d0d33c
soulayman <soulaymanjaa09@gmail.com>
2024-12-03 15:10:42 +0100

    players in squad (fin du temps)

--- FUT.js
+++ FUT.js
@@ -1,15 +1,18 @@
+let json ; 
 async function CallAPI() {
     let url = "/FUT.json";
     let fetcher = await fetch(url);
-    let json = await fetcher.json();
+    json = await fetcher.json();
     // console.log(json);
-    DisplayList(json);
-    const form = document.getElementById('addPlayerForm');
 
+    DisplayList(json);
+    
+const form = document.getElementById('addPlayerForm');
 form.addEventListener('submit', (e) => {
   e.preventDefault(); 
   const formData = new FormData(form);
   const obj = Object.fromEntries(formData); 
+console.log(obj);
 
   const newPlayer = {
     name: obj.name,       
@@ -23,14 +26,14 @@ form.addEventListener('submit', (e) => {
     passing: obj.passing,   
     dribbling: obj.dribbling,
     defending: obj.defending, 
-    physical: obj.physical
+    physical: obj.Physical
   };
   json.players.push(newPlayer);
    document.getElementById("addPlayerForm").reset();
 
 
   DisplayList(json);
-   console.log(json);
+//    console.log(json);
 });
 
 
@@ -117,6 +120,7 @@ function DisplayList(json) {
                             <img src="${players.photo}" class="img-fluid" alt="Player Photo" /></div>
                             <div class="col-md-8">
                             <p>${players.name}</p>
+
                             <div class=" col-md-2"><img src="${players.flag}" class="img-fluid py-lg-2" alt="country" /></div>
                              <div class=" col-md-2"><img src="${players.logo}" class="img-fluid py-lg-0" alt="club" /></div>
                             
@@ -176,15 +180,155 @@ function DisplayList(json) {
 // }
 
 
-function DisplayPlayerCard(json,i) {
+function DisplayPlayerCard(item) {
+    let choosedelementid;
+    choosedelementid = item.closest(".PlayerCard").children[0].id
+    // console.log(choosedelementid)
 
-    let SingleContainer = document.getElementById(`button${i}`)
-    SingleContainer.innerHTML = "";
-    SingleContainer.innerHTML = `
-    skjbkbtgkhgbkgthgbtkghbtg
-    `;
+filterdata(item.closest(".PlayerCard").children[0].id)
+
+
+document.querySelector(".Thelist").addEventListener("click" , function(e){
+    // console.log(choosedelementid)
+   let content  = `${e.target.closest("#ThePlayer").innerHTML}`
+   document.getElementById(choosedelementid).innerHTML = content
+}) 
+    
+
   
 }
+
+function filterdata(positionplayer){
+    json.players.forEach(players => {
+        if(players.position == positionplayer){
+            console.log(positionplayer)
+            let ListContainer = document.querySelector(".Thelist");
+            
+            let element; 
+            if (players.position == "GK") {
+                element = `
+                <div id="ThePlayer" class="row">
+           <div class="row">
+               <div class="col-md-1"></div>
+               <div class="col-md-11"> 
+           
+                <div class="container-fluid">
+               
+               <div class="BareOfTitles">
+               <div class="row">
+                   <div class="col-md-4">
+                           <div class="col-md-12">
+                           <div class="row">
+                               <div class="col-md-4">
+                               <img src="${players.photo}" class="img-fluid" alt="Player Photo" /></div>
+                               <div class="col-md-8">
+                               <p>${players.name}(GK)</p>
+                               <div class=" col-md-2"><img src="${players.flag}" class="img-fluid py-lg-2" alt="country" /></div>
+                                <div class=" col-md-2"><img src="${players.logo}" class="img-fluid py-lg-0" alt="club" /></div>
+                               
+                               </div>
+                               </div>
+                           </div>
+                           </div>
+                           <div class="col-md-8 py-lg-5">
+                               <div class="row">
+                               <div class="col-md-6">
+                                   <div class="row">
+                                   <div class="col-md-3 d-flex justify-content-center"><p>${players.rating}</p></div>
+                                   <div class="col-md-3 d-flex justify-content-center"><p>${players.position}</p></div>
+                                   <div class="col-md-3 d-flex justify-content-center"><p>${players.diving}</div>
+                                   <div class="col-md-3 d-flex justify-content-center"><p>${players.kicking}</div>
+                                   </div>
+                               </div>
+                               <div class="col-md-6">
+                                   <div class="row">
+                                   <div class="col-md-11">
+                                       <div class="row  d-flex justify-content-center">
+                                       <div class="col-md-3 d-flex justify-content-center"><p>${players.reflexes}</div>
+                                       <div class="col-md-3 d-flex justify-content-center"><p>${players.speed}</div>
+                                       <div class="col-md-3 d-flex justify-content-center"><p>${players.positioning}</div>
+                                       <div class="col-md-3 d-flex justify-content-center"><p>${players.handling}</div>
+                                   </div>
+                                   </div>
+                                   </div>
+                               </div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+               </div>
+           </div>
+       </div>
+       </div>
+    </div>
+           `} else {
+                element = `
+                 <div id="ThePlayer" class="row">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-11"> 
+            
+                 <div class="container-fluid">
+                
+                <div class="BareOfTitles">
+                <div class="row">
+                    <div class="col-md-4">
+                            <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-4">
+                                <img src="${players.photo}" class="img-fluid" alt="Player Photo" /></div>
+                                <div class="col-md-8">
+                                <p>${players.name}</p>
+    
+                                <div class=" col-md-2"><img src="${players.flag}" class="img-fluid py-lg-2" alt="country" /></div>
+                                 <div class=" col-md-2"><img src="${players.logo}" class="img-fluid py-lg-0" alt="club" /></div>
+                                
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                            <div class="col-md-8 py-lg-5">
+                                <div class="row">
+                                <div class="col-md-6">
+                                    <div class="row">
+                                    <div class="col-md-3 d-flex justify-content-center"><p>${players.rating}</p></div>
+                                    <div class="col-md-3 d-flex justify-content-center"><p>${players.position}</p></div>
+                                    <div class="col-md-3 d-flex justify-content-center"><p>${players.pace}</div>
+                                    <div class="col-md-3 d-flex justify-content-center"><p>${players.shooting}</div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="row">
+                                    <div class="col-md-11">
+                                        <div class="row  d-flex justify-content-center">
+                                        <div class="col-md-3 d-flex justify-content-center"><p>${players.passing}</div>
+                                        <div class="col-md-3 d-flex justify-content-center"><p>${players.dribbling}</div>
+                                        <div class="col-md-3 d-flex justify-content-center"><p>${players.defending}</div>
+                                        <div class="col-md-3 d-flex justify-content-center"><p>${players.physical}</div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>`}
+       ListContainer.innerHTML += element;
+
+        }
+
+    })
+
+
+}
+
 
 
 // const form = document.getElementById('addPèlayerForm');
